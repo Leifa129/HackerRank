91282222aa01274d9f30c88c444f62d5e7d87be1
Leifa129 <leif_xx@hotmail.com>
2019-12-13 14:07:04 +0100

    Startup

--- GreedyAlgorithms/GreedyFlorist.cs
+++ GreedyAlgorithms/GreedyFlorist.cs
@@ -1,7 +1,31 @@
 ﻿﻿namespace DefaultNamespace
 {
-    public class GreedyFlorist
+    /*
+     * https://www.hackerrank.com/challenges/greedy-florist/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=greedy-algorithms&h_r=next-challenge&h_v=zen
+     */
+ public class GreedyFlorist
     {
+        
+        // Complete the getMinimumCost function below.
+        static int getMinimumCost(int k, int[] c) {
+            int customers = 0;
+            int price = 0;
+            int multiplier = 1;
+            Array.Sort(c);
+
+            for(int i = c.Length - 1; i >= 0; i--){
+                if(customers == k){
+                    multiplier++;
+                    customers = 0;
+                }
+
+                price += c[i] * multiplier;
+                customers++;
+            }
+            
+            return price;
+        }
+
         
     }
 }
