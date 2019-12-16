ecd337913df711723e83911560b1df9d7f6a718c
Leifa129 <leif_xx@hotmail.com>
2019-12-16 14:40:02 +0100

    Staircase challenge

---
+++ Warmup/Staircase.cs
@@ -0,0 +1,25 @@
+﻿namespace DefaultNamespace
+{
+    /*
+     * Link to challenge
+     * https://www.hackerrank.com/challenges/staircase/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+     */
+
+public class Staircase
+    {
+        // Complete the staircase function below.
+        static void staircase(int n) {
+            int numberOfNs = 1;
+            for(int j = 0; j < n; j++){
+                for(int i = n; i > 0; i--){
+                    if(i > numberOfNs)
+                        Console.Write(" ");
+                    else
+                        Console.Write("#");
+                }
+                numberOfNs++;
+                Console.WriteLine();
+            }
+        }
+    }
+}
