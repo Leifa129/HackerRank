9f18f6ecb08903a7f0b53b4ae71d3e71433ee4ea
Leifa129 <leif_xx@hotmail.com>
2019-12-16 14:25:33 +0100

    Plus minus challenge

---
+++ Warmup/PlusMinus.cs
@@ -0,0 +1,34 @@
+﻿namespace DefaultNamespace
+{
+   /*
+    * Link to challenge
+    * https://www.hackerrank.com/challenges/plus-minus/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+    */
+
+public class PlusMinus
+    {
+        // Complete the plusMinus function below.
+        static void plusMinus(int[] arr) {
+            double negatives = 0;
+            double positives = 0;
+            double zeros = 0;
+
+            for(int i = 0; i < arr.Length; i++){
+                if(arr[i] > 0)
+                    positives++;
+
+                else if(arr[i] < 0)
+                    negatives++;
+
+                else zeros++;
+            }
+
+
+            Console.WriteLine(positives / arr.Length);
+            Console.WriteLine(negatives / arr.Length);
+            Console.WriteLine(zeros / arr.Length);
+
+
+        }
+    }
+}
