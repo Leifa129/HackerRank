170a2bd74460c2059340ec249f44def54e998991
Leifa129 <leif_xx@hotmail.com>
2019-12-16 15:27:46 +0100

    Minimum max sum

---
+++ Warmup/MiniMaxSum.cs
@@ -0,0 +1,32 @@
+﻿namespace DefaultNamespace
+{
+    /*
+     * https://www.hackerrank.com/challenges/mini-max-sum/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+     */
+    public class MiniMaxSum
+    {
+        // Complete the miniMaxSum function below.
+        // Find the maximum and minimum sum using 4 numbers in the array of 5.
+        static void miniMaxSum(int[] arr) {
+            long maxSum = 0;
+            long minSum = 0;
+            List<int> highest = new List<int>(arr);
+            List<int> lowest = new List<int>(arr);
+            // Amount of numbers we need to select
+            int n = 4;
+
+            highest.Sort();
+            highest.Reverse();
+
+            lowest.Sort();
+
+            for(int i = 0; i < n; i++){
+                maxSum += highest[i];
+                minSum += lowest[i];
+            }
+            Console.Write(minSum + " " + maxSum);
+                
+
+        }
+    }
+}
