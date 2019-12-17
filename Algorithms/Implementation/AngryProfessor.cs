b5f76599e0c2ec9ffe59d45c36a21ad000aadff9
Leifa129 <leif_xx@hotmail.com>
2019-12-17 14:03:11 +0100

    Angry professor

---
+++ Algorithms/Implementation/AngryProfessor.cs
@@ -0,0 +1,30 @@
+﻿namespace DefaultNamespace
+{
+    /*
+     * Link to challenge:
+     * https://www.hackerrank.com/challenges/angry-professor/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+     */
+    
+    public class AngryProfessor
+    {
+        // Complete the angryProfessor function below.
+        static string angryProfessor(int k, int[] a) {
+            int count = 0;
+            for(int i = 0; i < a.Length; i++){
+                if(a[i] <= 0){
+                    count++;
+                    if(count >= k){
+                        break;
+                    }
+                }
+            }
+
+            if(count < k)
+                return "YES";
+
+            else
+                return "NO";
+
+        }
+    }
+}
