4bf6407cddd6cc1ee789295348afa1f2d96f1f39
Leifa129 <leif_xx@hotmail.com>
2019-12-17 14:19:54 +0100

    Beautiful days

---
+++ Algorithms/Implementation/BeautifulDays.cs
@@ -0,0 +1,34 @@
+﻿namespace DefaultNamespace
+{
+    /*
+     * Link to challenge:
+     * shorturl.at/qxMP9
+     */
+    public class BeautifulDays
+    {
+        static int reverseNumber(int i){
+            int rem;
+            int reverse = 0;
+            while(i!=0)      
+            {      
+                rem=i%10;        
+                reverse=reverse*10+rem;      
+                i/=10;      
+            }      
+            return reverse;
+        }
+        // Complete the beautifulDays function below.
+        static int beautifulDays(int i, int j, int k) {
+            int count = 0;    
+            for(int n = i; n<=j; n++){
+                int reverse = reverseNumber(n);
+                if( (n - reverse) % k == 0){
+                    count++;
+                }
+            }
+
+            return count;
+
+        }
+    }
+}
