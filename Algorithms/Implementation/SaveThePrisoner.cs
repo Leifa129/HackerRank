9b3cfc61f248ecf8a840d41eacb2cc9ccadebdbf
Leifa129 <leif_xx@hotmail.com>
2019-12-17 15:33:39 +0100

    Save the prisoner

---
+++ Algorithms/Implementation/SaveThePrisoner.cs
@@ -0,0 +1,19 @@
+﻿namespace DefaultNamespace
+{
+    /*
+     * Link to challenge:
+     * shorturl.at/etAHI
+     */
+    public class SaveThePrisoner
+    {
+        // Complete the saveThePrisoner function below.
+        static int saveThePrisoner(int n, int m, int s) {
+            int initialIteration = (m % n);
+
+            if((initialIteration + s - 1) % n == 0)
+                return n;
+            else
+                return (initialIteration + s - 1) % n ;
+        }
+    }
+}
