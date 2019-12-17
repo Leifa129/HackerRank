3b11da823b784e2173d60de7aa40a1d534fe5844
Leifa129 <leif_xx@hotmail.com>
2019-12-17 15:10:17 +0100

    Viral advertising

---
+++ Algorithms/Implementation/ViralAdvertising.cs
@@ -0,0 +1,25 @@
+﻿namespace DefaultNamespace
+{
+    /*
+     * Link to challenge:
+     * shorturl.at/sO278
+     */
+    public class ViralAdvertising
+    {
+        // Complete the viralAdvertising function below.
+        static int viralAdvertising(int n) {
+            int shared = 5;
+            int liked = shared / 2;
+            int cumulative = liked;
+            for(int i = 0; i < n - 1; i++){
+                shared = liked  * 3;
+                liked = shared / 2;
+                cumulative += liked;
+            
+            
+            }
+            return cumulative;
+
+        }
+    }
+}
