3dd6bc8cf7684fcdfae329cd015af56dfe27a529
Leifa129 <leif_xx@hotmail.com>
2019-12-18 15:48:20 +0100

    Circular array rotation

---
+++ Algorithms/Implementation/CircularArrayRotation.cs
@@ -0,0 +1,35 @@
+﻿namespace DefaultNamespace
+{
+    /*
+     * Link to challenge:
+     * shorturl.at/cpO07
+     */
+    public class CircularArrayRotation
+    {
+        // Complete the circularArrayRotation function below.
+        static int[] circularArrayRotation(int[] a, int k, int[] queries) {
+
+            for(int j = 0; j < k; j++){
+                int temp = a[a.Length - 1];
+                for(int i = a.Length - 1; i >= 0; i--){
+                    if(i == 0){
+                        a[i] = temp;
+                    }
+                    else {
+                        a[i] = a[i - 1];
+                    }
+                }
+            }
+
+
+            int[] result = new int[queries.Length];
+            for(int i = 0; i < queries.Length; i++){
+                result[i] = a[queries[i]];
+            }
+
+
+            return result;
+
+        }
+    }
+}
