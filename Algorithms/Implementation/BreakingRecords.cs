d37fe53a9ee885aebf9ff4a73eb2df30609854ee
Leifa129 <leif_xx@hotmail.com>
2019-12-20 17:03:04 +0100

    Breaking records

---
+++ Algorithms/Implementation/BreakingRecords.cs
@@ -0,0 +1,33 @@
+﻿namespace DefaultNamespace
+{
+    /*
+     * Link to challenge:
+     *  shorturl.at/ilpLN
+     */
+    public class BreakingRecords
+    {
+        // Complete the breakingRecords function below.
+        static int[] breakingRecords(int[] scores) {
+            int minScore = scores[0];
+            int maxScore = scores[0];
+
+            int[] minMaxScoreCount = new int[2];
+
+            for(int i = 1; i < scores.Length; i++){
+                if(minScore < scores[i]){
+                    minMaxScoreCount[0]++;
+                    minScore = scores[i];
+                }
+
+                if(maxScore > scores[i]){
+                    minMaxScoreCount[1]++;
+                    maxScore = scores[i];
+                }
+
+            }
+
+            return minMaxScoreCount;
+
+        }
+    }
+}
