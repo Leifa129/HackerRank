42d3d41d32cc6abf87e24cf3ec485a36a36cfde7
Leifa129 <leif_xx@hotmail.com>
2019-12-20 15:45:22 +0100

    Kangaroo

---
+++ Algorithms/Implementation/Kangaroo.cs
@@ -0,0 +1,23 @@
+﻿namespace DefaultNamespace
+{
+    /*
+     * Link to challenge:
+     * https://www.hackerrank.com/challenges/kangaroo/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+     */
+    public class Kangaroo
+    {
+        // Complete the kangaroo function below.
+        static string kangaroo(int x1, int v1, int x2, int v2) {
+
+            while(x1 < x2){
+                x1 += v1;
+                x2 += v2;
+                if(x1 == x2)
+                    return "YES";
+            }
+
+            return "NO";
+
+        }
+    }
+}
